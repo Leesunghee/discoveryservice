f29a8c35cd5af28ad6b42a4ac910350bcb4d4c01
E590050 <realwunz@hyundai-autoever.com>
2021-07-14 21:18:16 +0900

    initail commit (Service Discovery)

---
+++ src/test/java/com/himalaya/discoveryservice/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himalaya.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
